83486cb7a9f0218d90fce06d0b97ff2697802447
naumov_evg <naumov_evg@clickon.ru>
2020-07-04 15:40:52 +0400

    подготовка к тесту

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,78 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+	<div id="app">
+		<button @click="showDeviceList = !showDeviceList">Show device list</button>
+		<div v-if="showDeviceList">
+			<div><b>Device list:</b></div>
+			<ul>
+				<li v-for="d in devices">
+					<ul>
+						<li><b>Device:</b> {{ d.deviceId }}</li>
+						<li><b>GroupId:</b> {{ d.groupId }}</li>
+						<li><b>Kind:</b> {{ d.kind }}</li>
+						<li><b>Label:</b> {{ d.label }}</li>
+					</ul>
+					<hr>
+				</li>
+			</ul>
+		</div>
+
+
+		<video ref="video"></video>
+	</div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+	import { Component, Vue } from 'vue-property-decorator';
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class App extends Vue {}
+	@Component
+	export default class App extends Vue {
+
+		showDeviceList = false;
+		devices: MediaDeviceInfo[] = [];
+
+		async mounted(){
+			this.devices = await this.getDevices()
+
+			let videoEl = this.$refs.video as HTMLVideoElement;
+			videoEl.srcObject = await this.getDepthStream();
+
+			// let stream =
+			// this.$refs['video'].
+		}
+		async getDevices(): Promise<MediaDeviceInfo[]> {
+			return await navigator.mediaDevices.enumerateDevices();
+		}
+
+		getConstraintsForDepthDevice() {
+			return new Promise<MediaStreamConstraints>(function(resolve, reject) {
+				navigator.mediaDevices.enumerateDevices()
+					.then(function(devices) {
+						for (let i = 0; i < devices.length; ++i) {
+							if (devices[i].label.indexOf("Depth") != -1) {
+								return resolve({video:{deviceId: {exact: devices[i].deviceId}}});
+							}
+						}
+						return reject("No depth device found");
+					})
+			});
+		}
+
+		async getDepthStream() {
+			let constraints = await this.getConstraintsForDepthDevice()
+			if (!constraints)
+				throw "No depth device found";
+
+			return navigator.mediaDevices.getUserMedia(constraints);
+		}
+	}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	video {
+		display: block;
+		margin-top: 50px;
+		width: 1024px;
+		max-width: 100%;
+		height: 768px;
+	}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+Vue.config.errorHandler = (err, vm, info) => {
+	alert(err)
+	console.error(err, vm, info)
+}
 
 new Vue({
   render: h => h(App),
